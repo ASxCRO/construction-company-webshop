bab050aceb10adb789804541bbee0d997779e046
Antonio Supan <antonio.supan@vsmti.hr>
2020-05-25 17:50:09 +0200

    skladiste dt + dodaj artikl

--- js/skladiste.js
+++ js/skladiste.js
@@ -28,3 +28,25 @@ function RedirectToDokumentiTab()
   $(dokumentiTabHeader).addClass('active');
   $(dokumentiTabContent).addClass('active');
 }
+
+
+$.extend( true, $.fn.dataTable.defaults, {
+  "ordering": false,
+      "info":     false,
+      "filter": false,
+      "search": false,
+      "lengthChange": true,
+      order: [[ 2, "desc" ]],
+      stateSave: true,
+      "language": {
+        "decimal": ",",
+        "thousands": ".",
+        "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Croatian.json"
+      },
+      "dom": '<"top"i>t<"bottom"flp><"clear">',
+      "lengthMenu": [[2, 4, 6,8], [2, 4, 6,8]],
+} );
+
+$(document).ready(function() {
+  $('#stanjeTablica').DataTable();
+});
